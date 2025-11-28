ed50efa6d63b1d235356ba49276ff15f59ee0c29
Etienne Samson <samson.etienne@gmail.com>
2025-11-28 21:59:39 +0100

    Hide the whole construction into an IIFE

--- src/screeps-browser-core.js
+++ src/screeps-browser-core.js
@@ -1,18 +1,66 @@
-window.DomHelper = window.DomHelper || {};
-(function(DomHelper) {
-    function waitForAngular() {
-        return new Promise(resolve => {
-            const check = () => {
-                const injector = angular.element(document.body).injector();
-                if (injector) {
-                    resolve(injector);
-                } else {
-                    setTimeout(check, 20);
-                }
-            };
-            check();
-        });
+(() => {
+
+    VERSION = "0.2";
+
+    /**
+     * @param {string} a
+     * @param {string} b
+     */
+    function compareVersion(a, b) {
+        const partsA = a.split(".");
+        const partsB = b.split(".");
+        while (partsA.length < partsB.length) partsA.push("0");
+        while (partsA.length > partsB.length) partsB.push("0");
+        return partsA.reduce((cmp, current, idx) => {
+            if (cmp !== 0) return cmp;
+            if (current !== partsB[idx])
+                return Math.sign(Number(current) - Number(partsB[idx]));
+            return 0;
+        }, 0);
     }
+
+    if (window.ScreepsAdapter && !window.ScreepsAdapter.VERSION) {
+        // This is unversioned adapter, just override
+    } else if (window.ScreepsAdapter && compareVersion(window.ScreepsAdapter.VERSION, VERSION) >= 0) {
+        // Already loaded a more recent version
+        return;
+    }
+
+    /**
+     * Polls every 50 milliseconds for a given condition
+     * @param {() => boolean} condition
+     * @param {number} [pollInterval=50]
+     * @param {number} timeoutAfter
+     */
+    async function waitFor(condition, pollInterval = 50, timeoutAfter) {
+        // Track the start time for timeout purposes
+        const startTime = Date.now();
+
+        while (true) {
+            // Check for timeout, bail if too much time passed
+            if(typeof(timeoutAfter) === 'number' && Date.now() > startTime + timeoutAfter) {
+                throw new Error('Condition not met before timeout');
+            }
+
+            // Check for conditon immediately
+            const result = await condition();
+
+            // If the condition is met...
+            if(result) {
+                // Return the result....
+                return result;
+            }
+
+            // Otherwise wait and check after pollInterval
+            await new Promise(r => setTimeout(r, pollInterval));
+        }
+    }
+
+    async function waitForAngular() {
+        await waitFor(() => angular.element(document.body).injector())
+    }
+
+    const DomHelper = {};
     DomHelper.addStyle = function (css) {
         let head = document.head;
         if (!head) return;
@@ -29,28 +77,13 @@ window.DomHelper = window.DomHelper || {};
         let $compile = parent.injector().get("$compile");
         return $compile(content)($scope);
     }
-})(DomHelper);
+    window.DomHelper = DomHelper;
 
-window.ScreepsAdapter = window.ScreepsAdapter || {};
-(function(ScreepsAdapter) {
-    function waitForAngular() {
-        return new Promise(resolve => {
-            const check = () => {
-                const injector = angular.element(document.body).injector();
-                if (injector) {
-                    resolve(injector);
-                } else {
-                    setTimeout(check, 20);
-                }
-            };
-            check();
-        });
-    }
-
+    const ScreepsAdapter = {};
+    ScreepsAdapter.VERSION = VERSION;
     // Listen for changes to the main screeps view
     // Examples: roomEntered, scriptClick, consoleClick, worldMapEntered, simulationMainMenu, gameLobby
     ScreepsAdapter.onViewChange = function (callback) {
-
         waitForAngular().then(() => {
             let rootScope = angular.element(document.body).scope();
             if (!rootScope.viewChangeCallbacks) {
@@ -73,6 +106,11 @@ window.ScreepsAdapter = window.ScreepsAdapter || {};
         });
     };
 
+    /**
+     * Execute a callback if the URL hash changes.
+     *
+     * @param {(hash: string) => void} callback
+     */
     ScreepsAdapter.onHashChange = function (callback) {
         waitForAngular().then(() => {
             const rootScope = angular.element(document.body).scope();
@@ -82,14 +120,18 @@ window.ScreepsAdapter = window.ScreepsAdapter || {};
                         rootScope.hashChangeCallbacks[i](window.location.hash);
                     }
                 });
-                
+
                 rootScope.hashChangeCallbacks = [];
             }
-            
+
             rootScope.hashChangeCallbacks.push(callback);
         });
     };
 
+    /**
+     * Execute a callback if the current room changes.
+     * @param {(roomName: string) => void} callback
+     */
     ScreepsAdapter.onRoomChange = function (callback) {
         ScreepsAdapter.onHashChange((hash) => {
             let rootScope = angular.element(document.body).scope();
@@ -123,4 +165,6 @@ window.ScreepsAdapter = window.ScreepsAdapter || {};
             configurable: true
         });
     });
-})(ScreepsAdapter);
+
+    window.ScreepsAdapter = ScreepsAdapter;
+})();
