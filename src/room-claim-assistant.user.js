bef8b9a3338b233a1d8bee5a374ca2e1a5833af6
VexingRaven <msgerbs@users.noreply.github.com>
2026-03-16 11:53:23 -0500

    Recommended status ignores presence of Invader.

--- src/room-claim-assistant.user.js
+++ src/room-claim-assistant.user.js
@@ -1,20 +1,20 @@
 // ==UserScript==
-// @name         Screeps room claim assistant
-// @namespace    https://screeps.com/
-// @version      0.1.8
-// @author       James Cook
-// @match        https://screeps.com/a/*
-// @match        https://screeps.com/ptr/*
-// @match        https://screeps.com/season/*
-// @match        http://*.localhost:*/(*)/#!/*
-// @run-at       document-ready
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=screeps.com
-// @require      REPO_URL/screeps-browser-core.js
-// @downloadUrl  REPO_URL/room-claim-assistant.user.js
-// @grant        GM_getValue
-// @grant        GM_setValue
-// @grant        GM_registerMenuCommand
-// @grant        GM_unregisterMenuCommand
+// @name        Screeps room claim assistant
+// @namespace   https://screeps.com/
+// @version     0.1.7
+// @author      James Cook
+// @match       https://screeps.com/a/*
+// @match       https://screeps.com/ptr/*
+// @match       https://screeps.com/season/*
+// @match       http://*.localhost:*/(*)/#!/*
+// @run-at      document-ready
+// @icon        https://www.google.com/s2/favicons?sz=64&domain=screeps.com
+// @require     https://screepers.github.io/screeps-browser-ext/screeps-browser-core.js?v=1773232591774
+// @downloadUrl https://screepers.github.io/screeps-browser-ext/room-claim-assistant.user.js?v=1773232591774
+// @grant GM_getValue
+// @grant GM_setValue
+// @grant GM_registerMenuCommand
+// @grant GM_unregisterMenuCommand
 // ==/UserScript==
 
 let ignoreSigns = GM_getValue('ignoreSigns');
@@ -106,6 +106,7 @@ function recalculateClaimOverlay() {
                 }
 
                 let userOwned = (roomStats.own && roomStats.own.user === user._id);
+                let invaderOwned = (roomStats.own && roomStats.own.user === "2"); // 2 is the hardcoded ID for Invader
 
                 // show minerals if:
                 let showMinerals =
@@ -115,14 +116,14 @@ function recalculateClaimOverlay() {
                 let state = "not-recommended";
                 if (userOwned && roomStats.own.level > 0) {
                     state = "owned";
-                } else if (roomStats.own && !userOwned) {
-                    state = "prohibited";
+                } else if (roomStats.own && !userOwned && !invaderOwned) {
+                    state = "prohibited"; // rooms reserved or claimed by anyone except the user or Invader
                 } else if (!ignoreSigns && roomStats.sign && !userOwned && roomStats.sign.user !== user._id) {
                     state = "signed";
                 } else if (counts.c.length === 0) {
                     state = "unclaimable";
                 } else if (counts.s.length >= 2 &&
-                    (!roomStats.own || (userOwned && roomStats.own.level === 0))) {
+                    (!roomStats.own || (userOwned && roomStats.own.level === 0) || invaderOwned)) {
                     // recommend if it has two sources and a controller, nobody else owns it,
                     // and user hasn't already claimed
                     state = "recommended";
